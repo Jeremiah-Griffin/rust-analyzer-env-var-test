512c321206d1387650b1d5ea37ad7d4c7feb524a
Jeremiah Griffin <jeremiahgriffin@protonmail.com>
2024-07-21 19:38:43 -0400

    improved comments

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ fn main() {
     //This does not return an error from the database but refuses to compile, complaining that DATABASE_URL is unset...
     query!("SELECT bar FROM foo");
     //This *should not* compile.
-    //...this *does* return an error from the database meaning the connection must exist, but rust analyzer is doing something very strange.
-    query!("SELECT baz FROM foo");
+    //...this *does* return an error from the database meaning the connection must exist, but rust analyzer still says DATABASE_URL is unset.
+    //how can the macro not find the database *and* capture errors from a database it cannot find?
+    //check "schroedingers_connection.png for a screenhot of this behavior.
+    //query!("SELECT baz FROM foo");
+
     //Perhaps RA is running proc macros twice? Once with the environment variables set in config.toml and once without?
 
     //Other elements of config.toml are respected...
